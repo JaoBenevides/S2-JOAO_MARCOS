80aa6d0a3c8ea1fff79c707768c43f5883ff8742
JaoBenevides <j.benevides@aluno.senai.br>
2025-04-08 08:50:03 -0300

    Update convert.py
    Atualização na Branch main

---
+++ Convert_Temperatura/convert.py
@@ -0,0 +1,15 @@
+print("Converta a temperatura de Celsius(1) para Fahrenheit(2) e vice-versa")
+
+lot = int(input("Digite qual temperatura sera convertida: "))
+
+def v(lot):
+    if lot == 1:
+      jao = (celsius*9/5)+32
+      
+
+    else:
+        lot == 2
+        jao2 = (Fahrenheit-32)*5/9
+        
+    
+
